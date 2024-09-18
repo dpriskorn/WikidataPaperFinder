47c6549028bb54d1cd2a2d738cb2d30933b65ce4
Dennis Priskorn <egils@riseup.net>
2024-09-18 12:12:20 +0200

    chore: Run black

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,16 +2,21 @@
 import pytest
 from app import app
 
+
 @pytest.fixture()
 def test_app():
-    app.config.update({
-        "TESTING": True,
-    })
+    app.config.update(
+        {
+            "TESTING": True,
+        }
+    )
     yield app
+
 
 @pytest.fixture()
 def client(test_app):
     return test_app.test_client()
+
 
 @pytest.fixture()
 def runner(test_app):
